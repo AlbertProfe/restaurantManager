e2b68d9adc5483c6d6cb77ba7c5b487b9f508b25
Emma Alonso <emmcoy7@gmail.com>
2024-10-18 13:17:39 +0200

    Delete additional loaders

--- src/main/java/dev/example/restaurantManager/RestaurantManagerApplication.java
+++ src/main/java/dev/example/restaurantManager/RestaurantManagerApplication.java
@@ -1,20 +1,11 @@
 package dev.example.restaurantManager;
 
-import dev.example.restaurantManager.utilities.CustomerDataLoader;
-import dev.example.restaurantManager.utilities.DataLoader;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 
 //@PropertySource("classpath:application.properties")
 @SpringBootApplication
 public class RestaurantManagerApplication {
-
-	@Autowired
-	private DataLoader dataLoader;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantManagerApplication.class, args);
