05e2841ad50b6cb828a71f176b08f9e6175048c9
AlberteMB <albertebardanca@gmail.com>
2024-10-20 12:42:48 +0200

    - [X] Update Entity Classe - [X] Create Faker Classe - [X] Develop Service Layer - [X] Implement JPA Repositories - [X] Design REST Controllers - [ ] Test with Swagger API
    - application.properties changed

--- src/main/java/dev/example/restaurantManager/RestaurantManagerApplication.java
+++ src/main/java/dev/example/restaurantManager/RestaurantManagerApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class RestaurantManagerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestaurantManagerApplication.class, args);
 	}
 
