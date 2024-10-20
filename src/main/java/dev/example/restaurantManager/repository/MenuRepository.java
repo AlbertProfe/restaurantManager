738184d62af4596956384d3a82c207d417aff0f6
AlberteMB <albertebardanca@gmail.com>
2024-10-20 09:59:37 +0200

    - [X] Update Entity Classes - [X] Create Faker Classes - [X] Develop Service Layer - [X] Implement JPA Repositories - [X] Design REST Controllers - [ ] Test with Swagger API
    - JPA Repositories

--- src/main/java/dev/example/restaurantManager/repository/MenuRepository.java
+++ src/main/java/dev/example/restaurantManager/repository/MenuRepository.java
@@ -6,24 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository <MenuRestaurant, String> {
-
-
-    // 0. Count menus by name
-    int countByName(String name);
-    // 1. Find menus by name
-    ArrayList<MenuRestaurant> findByName (String name);
-    // 2. Find menus by price
-    ArrayList<MenuRestaurant> findByPrice (Double price);
-    // 3. Find menus by content
-    ArrayList<MenuRestaurant> findByContent (String content);
-    // 4. Find menus by status active
-    ArrayList<MenuRestaurant> findByActive (boolean active);
-    // 5. Find menus by containing water
-    ArrayList<MenuRestaurant> findByWater (boolean water);
-    // 6. Find menus by price less than a specified value and not deleted
-    List<MenuRestaurant> findByPriceLessThanAndDeletedFalse (int price);
-    // 7. Find menus by price more than a specified value and not deleted
-    List<MenuRestaurant> findByPriceMoreThanAndDeletedFalse (int price);
+//
+//
+//    // 0. Count menus by name
+//    int countByName(String name);
+//    // 1. Find menus by name
+//    ArrayList<MenuRestaurant> findByName (String name);
+//    // 2. Find menus by price
+//    ArrayList<MenuRestaurant> findByPrice (Double price);
+//    // 3. Find menus by content
+//    ArrayList<MenuRestaurant> findByContent (String content);
+//    // 4. Find menus by status active
+//    ArrayList<MenuRestaurant> findByActive (boolean active);
+//    // 5. Find menus by containing water
+//    ArrayList<MenuRestaurant> findByWater (boolean water);
+//    // 6. Find menus by price less than a specified value and not deleted
+//    List<MenuRestaurant> findByPriceLessThanAndDeletedFalse (int price);
+//    // 7. Find menus by price more than a specified value and not deleted
+//    List<MenuRestaurant> findByPriceMoreThanAndDeletedFalse (int price);
 
 
 }
--- src/main/java/dev/example/restaurantManager/repository/TableRepository.java
+++ src/main/java/dev/example/restaurantManager/repository/TableRepository.java
@@ -9,15 +9,15 @@ import java.util.List;
 
 public interface TableRepository extends JpaRepository <TableRestaurant, String> {
 
-    // 0. Count tables by name
-    int countByName(String name);
-    // 1. Find tables by name
-    ArrayList<TableRestaurant> findByName(String name);
-    // 2. Find tables by quantity
-    ArrayList<TableRestaurant> FindByQty(int qty);
-    // 3. Find tables by busy or not
-    List<TableRestaurant> findByBusy(boolean busy);
-
-
+//    // 0. Count tables by name
+//    int countByName(String name);
+//    // 1. Find tables by name
+//    ArrayList<TableRestaurant> findByName(String name);
+//    // 2. Find tables by quantity
+//    ArrayList<TableRestaurant> FindByQty(int qty);
+//    // 3. Find tables by busy or not
+//    List<TableRestaurant> findByBusy(boolean busy);
+//
+//
 
 }
