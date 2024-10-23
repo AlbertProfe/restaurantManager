37361663af75f6b62cd9338fd19afc11fc0de41b
jc-programs <jose@gueto.com>
2024-10-23 17:31:58 +0200

    added application-jc-local.properties

--- src/main/java/dev/example/restaurantManager/model/EatInOrderRestaurant.java
+++ src/main/java/dev/example/restaurantManager/model/EatInOrderRestaurant.java
@@ -15,21 +15,22 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class EatInOrderRestaurant extends OrderRestaurant {
 
-    private ArrayList<TableRestaurant> tableRestaurants = new ArrayList<>();
-    //private TableRestaurant orderedTableRestaurant;
+    //private ArrayList<TableRestaurant> tableRestaurants = new ArrayList<>();
+    private TableRestaurant tableEatInOrder;
 
 
     public EatInOrderRestaurant(String id, Date date, String waiter, int peopleQty,
                                 double totalPayment, boolean paid, ArrayList<Menu> menus,
-                                ArrayList<TableRestaurant> tableRestaurants) {
+                                TableRestaurant tableEatInOrder) {
         super(id, date, waiter, peopleQty, totalPayment, paid, menus);
-        this.tableRestaurants = tableRestaurants;
+        this.tableEatInOrder = tableEatInOrder;
     }
 
     @Override
     public String toString() {
         return super.toString() + "\n" +
                 "Type: Eat In\n" +
-                "Tables: " + tableRestaurants.stream().map(TableRestaurant::getName).collect(Collectors.joining(", "));
+                "Table EatInOrder: " + tableEatInOrder;
+//                "Tables: " + tableRestaurants.stream().map(TableRestaurant::getName).collect(Collectors.joining(", "));
     }
 }
