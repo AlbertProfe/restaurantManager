c8298e91185a78f8bf70b941d5f3f27eec99db0e
isaac <isaac.diez@proton.me>
2024-11-16 19:29:40 +0100

    Update MenuItemRepository

--- src/main/java/dev/example/restaurantManager/repository/MenuItemRepository.java
+++ src/main/java/dev/example/restaurantManager/repository/MenuItemRepository.java
@@ -1,9 +1,23 @@
 package dev.example.restaurantManager.repository;
 
+
+import dev.example.restaurantManager.model.MainCourse;
 import dev.example.restaurantManager.model.MenuItem;
+import dev.example.restaurantManager.model.SideCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface MenuItemRepository extends JpaRepository<MenuItem,String> {
+import java.util.List;
+
+    public interface MenuItemRepository extends JpaRepository<MenuItem, String> {
 
 
-}
+        @Query("SELECT m FROM MainCourse m")
+        List<MainCourse> findAllMainCourses();
+
+        @Query("SELECT s FROM SideCourse s")
+        List<SideCourse> findAllSideCourses();
+
+    }
+
+
